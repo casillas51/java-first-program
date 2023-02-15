25ff1f85062a981291ecf2a8fae4dbd6bdd54614
casillas51 <casillas51@gmail.com>
2023-02-14 18:59:25 -0600

    Complete module

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -17,7 +17,7 @@ public class BestLoanRates {
         float bestRate = getRates(loanTermInYears);
 
         if (bestRate == 0.0f) {
-            System.out.println("No available rates for term: " + loanTermInYears);
+            System.out.println("No available rates for term: " + loanTermInYears + " years");
         } else {
             System.out.println("Best Available Rate: " + bestRate + "%");
         }
